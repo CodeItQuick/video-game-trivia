fc90dee179301a89ac3d45c15537748c8e4aff5d
CodeItQuick <evanontario@hotmail.com>
2025-12-31 12:26:28 -0400

    add vercel.json

--- server/src/app.js
+++ server/src/app.js
@@ -2,72 +2,62 @@
 const cors = require('@fastify/cors');
 const {GameRunner} = require('./domain/dist/game-runner.js');
 
-function init() {
-    const fastify = Fastify({
-        logger: true
-    })
-    fastify.register(cors, {
-        origin: "https://video-game-trivia-codeitquicks-projects.vercel.app/"
-    })
+const fastify = Fastify({
+    logger: true
+})
+fastify.register(cors, {
+    origin: "https://video-game-trivia-codeitquicks-projects.vercel.app/"
+})
 
-    let gameRunner, game, winner = false, running;
-    const runner = () => ({
-        names: [],
-        numPlayers: [],
-        answers: [],
-        promptNames(_numberOfPlayers) {
-            return this.names;
-        },
-        promptNumber(_lowerBound, _upperBound, _queryName) {
-            return this.numPlayers.shift() || 1;
-        },
-        promptAnswer(_lowerBound, _upperBound, _queryName) {
-            return this.answers.shift();
-        }
-    });
+let gameRunner, game, winner = false, running;
+const runner = () => ({
+    names: [],
+    numPlayers: [],
+    answers: [],
+    promptNames(_numberOfPlayers) {
+        return this.names;
+    },
+    promptNumber(_lowerBound, _upperBound, _queryName) {
+        return this.numPlayers.shift() || 1;
+    },
+    promptAnswer(_lowerBound, _upperBound, _queryName) {
+        return this.answers.shift();
+    }
+});
 
 // Declare a route
-    fastify.get('/', async function (request, reply) {
-
-
-        [gameRunner, game] = await GameRunner.main(console, runner());
-        gameRunner.names = [];
-        gameRunner.numPlayers = 0;
-        gameRunner.answers = [];
-        winner = false;
-        reply.send({gameInstanceMade: !!(gameRunner && game)})
-    })
+fastify.get('/', async function (request, reply) {
+    [gameRunner, game] = await GameRunner.main(console, runner());
+    gameRunner.names = [];
+    gameRunner.numPlayers = 0;
+    gameRunner.answers = [];
+    winner = false;
+    reply.send({gameInstanceMade: !!(gameRunner && game)})
+})
 // Declare a route
-    fastify.get('/create-new-game', async function (request, reply) {
+fastify.get('/create-new-game', async function (request, reply) {
 
-        gameRunner.names = ["Jeff", "Evan", "Chet"]
-        gameRunner.numPlayers = [3];
+    gameRunner.names = ["Jeff", "Evan", "Chet"]
+    gameRunner.numPlayers = [3];
 
-        const gameCreated = await GameRunner.createNewGame(gameRunner, game);
-        reply.send({newGameCreated: !!gameCreated})
-    })
+    const gameCreated = await GameRunner.createNewGame(gameRunner, game);
+    reply.send({newGameCreated: !!gameCreated})
+})
 // Declare a route
-    fastify.get('/take-turn-in', async function (request, reply) {
-        gameRunner.answers = [3];
-        if (!winner) {
-            winner = await GameRunner.takeTurnsIn(game, gameRunner);
-        }
-        reply.send({gameOver: winner})
+fastify.get('/take-turn-in', async function (request, reply) {
+    gameRunner.answers = [3];
+    if (!winner) {
+        winner = await GameRunner.takeTurnsIn(game, gameRunner);
+    }
+    reply.send({gameOver: winner})
 
-    });
-    return fastify;
-}
+});
 
-if (require.main === module) {
-    // called directly i.e. "node app"
-    init().then(x => x.listen({port: 3000}, function (err, address) {
-        if (err) {
-            Fastify.log.error(err)
-            process.exit(1)
-        }
-        // Server is now listening on ${address}
-    }));
-} else {
-    // required as a module => executed on aws lambda
-    module.exports = init;
-}
+// called directly i.e. "node app"
+fastify.listen({port: 3000}, function (err, address) {
+    if (err) {
+        Fastify.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
+});
