57aa3915822ed5da0717e774b65b1f9a34aed62d
CodeItQuick <evanontario@hotmail.com>
2025-12-31 10:52:51 -0400

    MVP closer to done with first vertical slice

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -45,22 +45,6 @@ console.log(gameOver)
     <div v-if="gameInstance && playersAdded && gameOver">Game is over!</div>
     <button type="button" v-if="gameInstance && playersAdded && gameOver" @click="requestNewInstance">Create New Game</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
